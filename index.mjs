edae6746291c62842bcd8cf48bb74dde90a88c49
Elia Lazzari <elia.lazzari@gmail.com>
2022-04-08 18:27:09 +0200

    Added implementation of console-gui-tools lib

--- index.mjs
+++ index.mjs
@@ -1,12 +1,8 @@
-#!/usr/bin/env node
- // For colors in the console
+// For colors in the console
 import chalk from 'chalk';
 chalk.level = 1
 import net from 'net';
 import EventEmitter from 'events';
-import readline from 'readline';
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
 
 const PORT = 9090
 const HOST = '127.0.0.1'
@@ -15,12 +11,16 @@ let period = 100
 let mode = 'random'
 
 const periodList = [10, 100, 250, 500, 1000, 2000, 5000, 10000]
-let selectedPeriod = period
-
 const modeList = ["random", "linear"]
-let selectedMode = mode
 
 const clientManager = new EventEmitter()
+
+import { ConsoleManager, OptionPopup, InputPopup } from 'console-gui-tools'
+const GUI = new ConsoleManager({
+    title: 'TCP Simulator', // Title of the console
+    logsPageSize: 8, // Number of lines to show in logs page
+    changeLayoutKey: 'ctrl+l', // Change layout with ctrl+l to switch to the logs page
+})
 
 let connectedClients = 0
     // The number of TCP message sent since start
@@ -46,14 +46,12 @@ const server = net.createServer(socket => {
 let lastErr = ""
 
 server.on('error', err => {
-    lastErr = chalk.bgRed("Error: ") + chalk.white(` ${err.message}`);
+    lastErr = chalk.red("Error: ") + chalk.white(` ${err.message}`);
+    GUI.error(lastErr)
 })
 
 let min = 9
 let max = 12
-
-let typedMaxValue = 0
-let typedMinValue = 0
 
 let values = [0, 0, 0, 0, 0, 0]
 
@@ -99,322 +97,110 @@ const sendValuesAsCsv = async() => {
 
 let window = "HOME"
 
-const updateConsoleGauge = async() => {
-    console.clear()
-    console.log(chalk.yellow(`TCP server simulator app! Welcome...`))
-    console.log(chalk.green(`TCP Server listening on ${HOST}:${PORT}`));
-    console.log(chalk.green(`Connected clients: `) + chalk.white(`${connectedClients}\n`));
-    console.log(chalk.magenta(`TCP Messages sent: `) + chalk.white(`${tcpCounter}`) + `\n`);
+/**
+ * @description Updates the console screen
+ *
+ */
+const updateConsole = async() => {
+    let screen = ""
+    screen += chalk.yellow(`TCP server simulator app! Welcome...`) + `\n`
+    screen += chalk.green(`TCP Server listening on ${HOST}:${PORT}`) + `\n`
+    screen += chalk.green(`Connected clients: `) + chalk.white(`${connectedClients}\n`)
+    screen += chalk.magenta(`TCP Messages sent: `) + chalk.white(`${tcpCounter}`) + `\n\n`
 
     // Print if simulator is running or not
     if (!valueEmitter) {
-        console.log(chalk.red(`Simulator is not running! `) + chalk.white(`press 'space' to start`))
+        screen += chalk.red(`Simulator is not running! `) + chalk.white(`press 'space' to start`) + `\n`
     } else {
-        console.log(chalk.green(`Simulator is running! `) + chalk.white(`press 'space' to stop`))
+        screen += chalk.green(`Simulator is running! `) + chalk.white(`press 'space' to stop`) + `\n`
     }
     // Print mode:
-    console.log(chalk.cyan(`Mode:`) + chalk.white(` ${mode}`));
+    screen += chalk.cyan(`Mode:`) + chalk.white(` ${mode}`) + `\n`;
     // Print message frequency:
-    console.log(chalk.cyan(`Message period:`) + chalk.white(` ${period} ms`));
+    screen += chalk.cyan(`Message period:`) + chalk.white(` ${period} ms`) + `\n`;
     // Print Min and Max
-    console.log(chalk.cyan(`Min:`) + chalk.white(` ${min}`));
-    console.log(chalk.cyan(`Max:`) + chalk.white(` ${max}`));
+    screen += chalk.cyan(`Min:`) + chalk.white(` ${min}`) + `\n`;
+    screen += chalk.cyan(`Max:`) + chalk.white(` ${max}`) + `\n`;
     // Print current values:
-    console.log(chalk.cyan(`Values:`) + chalk.white(` ${values.map(v => v.toFixed(4)).join('   ')}`));
+    screen += chalk.cyan(`Values:`) + chalk.white(` ${values.map(v => v.toFixed(4)).join('   ')}`) + `\n`;
 
     // Spacer
-    console.log(`\n`);
+    screen += `\n\n`;
 
     if (lastErr.length > 0) {
-        console.error(lastErr)
-        console.log('\n')
+        screen += lastErr + `\n\n`
     }
 
-    console.log(chalk.bgBlack(`Commands:`));
-    console.log(`  ${chalk.bold('space')}   - ${chalk.italic('Start/stop simulator')}`);
-    console.log(`  ${chalk.bold('m')}       - ${chalk.italic('Select simulation mode')}`);
-    console.log(`  ${chalk.bold('s')}       - ${chalk.italic('Select message period')}`);
-    console.log(`  ${chalk.bold('h')}       - ${chalk.italic('Set max value')}`);
-    console.log(`  ${chalk.bold('l')}       - ${chalk.italic('Set min value')}`);
-    console.log(`  ${chalk.bold('q')}       - ${chalk.italic('Quit')}`);
+    screen += chalk.bgBlack(`Commands:`) + `\n`;
+    screen += `  ${chalk.bold('space')}   - ${chalk.italic('Start/stop simulator')}\n`;
+    screen += `  ${chalk.bold('m')}       - ${chalk.italic('Select simulation mode')}\n`;
+    screen += `  ${chalk.bold('s')}       - ${chalk.italic('Select message period')}\n`;
+    screen += `  ${chalk.bold('h')}       - ${chalk.italic('Set max value')}\n`;
+    screen += `  ${chalk.bold('l')}       - ${chalk.italic('Set min value')}\n`;
+    screen += `  ${chalk.bold('q')}       - ${chalk.italic('Quit')}\n`;
+
+    GUI.setHomePage(screen)
 }
 
-const addOptionPopupLayer = (title, options, selected) => {
-    const offset = 2
-    const Terminal = process.stdout;
-    const maxOptionsLength = options.reduce((max, option) => Math.max(max, option.toString().length), 0)
-    const windowWidth = maxOptionsLength > title.length ? maxOptionsLength + (2 * offset) : title.length + (2 * offset)
+GUI.on("exit", () => {
+    closeApp()
+})
 
-    let header = "┌"
-    for (let i = 0; i < windowWidth; i++) {
-        header += "─"
-    }
-    header += "┐\n"
-    header += `│${" ".repeat((windowWidth - title.length) / 2)}${title}${" ".repeat((windowWidth - title.length) / 2)}│\n`
-    header += "├" + "─".repeat(windowWidth) + "┤\n"
-
-    //││
-    let footer = "└"
-    for (let i = 0; i < windowWidth; i++) {
-        footer += "─"
-    }
-    footer += "┘\n"
-
-    let content = ""
-    options.forEach((option, index) => {
-        content += `│${option === selected ? "<" : " "} ${option}${option === selected ? " >" : "  "}${" ".repeat(windowWidth - option.toString().length - 4)}│\n`
-    })
-
-    const windowDesign = `${header}${content}${footer}`
-    windowDesign.split('\n').forEach((line, index) => {
-        Terminal.cursorTo(Math.round((Terminal.columns / 2) - (windowWidth / 2)), 4 + index)
-        Terminal.write(line)
-    })
-}
-
-const addInputPopupLayer = (title, value) => {
-    const offset = 2
-    const Terminal = process.stdout;
-    const windowWidth = title.length > value.toString().length ? title.length + (2 * offset) : value.toString().length + (2 * offset)
-
-    let header = "┌"
-    for (let i = 0; i < windowWidth; i++) {
-        header += "─"
-    }
-    header += "┐\n"
-    header += `│${" ".repeat(windowWidth % 2 ? Math.round((windowWidth - title.length) / 2) : Math.round((windowWidth - title.length) / 2) - 1)}${title}${" ".repeat(Math.round((windowWidth - title.length) / 2))}│\n`
-    header += "├" + "─".repeat(windowWidth) + "┤\n"
-
-    //││
-    let footer = "└"
-    for (let i = 0; i < windowWidth; i++) {
-        footer += "─"
-    }
-    footer += "┘\n"
-
-    let content = ""
-        // Draw an input field
-    content += `│${"> "}${value}${" ".repeat(windowWidth - value.toString().length - 2)}│\n`
-
-    const windowDesign = `${header}${content}${footer}`
-    windowDesign.split('\n').forEach((line, index) => {
-        Terminal.cursorTo(Math.round((Terminal.columns / 2) - (windowWidth / 2)), 4 + index)
-        Terminal.write(line)
-    })
-    Terminal.cursorTo(Math.round((Terminal.columns / 2) - (windowWidth / 2)) + 2 + value.toString().length, 4 + 3)
-}
-
-// Add a command input listener to change mode
-process.stdin.on('keypress', (str, key) => {
-    if (key.ctrl && key.name === 'c') {
-        clearInterval(valueEmitter)
-        server.close()
-        process.exit()
-    }
-    switch (window) {
-        case "HOME":
-            {
-                switch (key.name) {
-                    case 'm':
-                        window = "SET_MODE"
-                        selectedMode = mode
-                        break
-                    case 'space':
-                        {
-                            if (!valueEmitter) {
-                                if (connectedClients > 0) {
-                                    valueEmitter = setInterval(frame, period)
-                                    if (lastErr.includes("No clients connected!")) {
-                                        lastErr = ""
-                                    }
-                                } else {
-                                    lastErr = chalk.redBright("Error: ") + chalk.white(`No clients connected!`);
-                                }
-                            } else {
-                                clearInterval(valueEmitter)
-                                valueEmitter = null
-                            }
-                        }
-                        break
-                    case 's':
-                        window = "SET_SPEED"
-                        selectedPeriod = period
-                        break
-                    case 'h':
-                        window = "SET_MAX"
-                        typedMaxValue = max
-                        break
-                    case 'l':
-                        window = "SET_MIN"
-                        typedMinValue = min
-                        break
-                    case 'q':
-                        clearInterval(valueEmitter)
-                        server.close()
-                        process.exit()
-                    default:
-                        break
-                }
+GUI.on("keypressed", (key) => {
+    switch (key.name) {
+        case 'space':
+            if (valueEmitter) {
+                clearInterval(valueEmitter)
+                valueEmitter = null
+            } else {
+                valueEmitter = setInterval(frame, period)
             }
             break
-        case "SET_SPEED":
-            {
-                switch (key.name) {
-                    case 'down':
-                        selectedPeriod = periodList[periodList.indexOf(selectedPeriod) + 1]
-                        break
-                    case 'up':
-                        selectedPeriod = periodList[periodList.indexOf(selectedPeriod) - 1]
-                        break
-                    case 'return':
-                        {
-                            period = selectedPeriod
-                            if (valueEmitter) {
-                                clearInterval(valueEmitter)
-                                valueEmitter = setInterval(frame, period)
-                            }
-                            window = "HOME"
-                        }
-                        break
-                    case 'escape':
-                        window = "HOME"
-                        break
-                    case 'q':
-                        clearInterval(valueEmitter)
-                        server.close()
-                        process.exit()
-                    default:
-                        break
-                }
-            }
+        case 'm':
+            new OptionPopup("popupSelectMode", "Select simulation mode", modeList, mode).show().on("confirm", (_mode) => {
+                mode = _mode
+                GUI.warn(`NEW MODE: ${mode}`)
+                drawGui()
+            })
             break
-        case "SET_MODE":
-            {
-                switch (key.name) {
-                    case 'down':
-                        selectedMode = modeList[modeList.indexOf(selectedMode) + 1]
-                        break
-                    case 'up':
-                        selectedMode = modeList[modeList.indexOf(selectedMode) - 1]
-                        break
-                    case 'return':
-                        {
-                            mode = selectedMode
-                            window = "HOME"
-                        }
-                        break
-                    case 'escape':
-                        window = "HOME"
-                        break
-                    case 'q':
-                        clearInterval(valueEmitter)
-                        server.close()
-                        process.exit()
-                    default:
-                        break
-                }
-            }
+        case 's':
+            new OptionPopup("popupSelectPeriod", "Select simulation period", periodList, period).show().on("confirm", (_period) => {
+                period = _period
+                GUI.warn(`NEW PERIOD: ${period}`)
+                drawGui()
+            })
             break
-        case "SET_MAX":
-            {
-                if (!Number.isNaN(Number(key.name))) {
-                    if (typedMaxValue.toString().length < 20) {
-                        let tmp = typedMaxValue.toString()
-                        tmp += key.name
-                        typedMaxValue = Number(tmp)
-                    }
-                } else if (key.sequence === '-') {
-                    typedMaxValue = typedMaxValue * -1
-                } else if (key.sequence === '+') {
-                    typedMaxValue = Math.abs(typedMaxValue)
-                } else {
-                    switch (key.name) {
-                        case 'backspace':
-                            if (typedMaxValue.toString().length > 0) {
-                                typedMaxValue = Number(typedMaxValue.toString().slice(0, typedMaxValue.toString().length - 1))
-                            }
-                            break
-                        case 'return':
-                            {
-                                max = typedMaxValue
-                                window = "HOME"
-                            }
-                            break
-                        case 'escape':
-                            window = "HOME"
-                            break
-                        case 'q':
-                            clearInterval(valueEmitter)
-                            server.close()
-                            process.exit()
-                        default:
-                            break
-                    }
-                }
-            }
+        case 'h':
+            new InputPopup("popupTypeMax", "Type max value", max, true).show().on("confirm", (_max) => {
+                max = _max
+                GUI.warn(`NEW MAX VALUE: ${max}`)
+                drawGui()
+            })
             break
-        case "SET_MIN":
-            {
-                if (!Number.isNaN(Number(key.name))) {
-                    if (typedMinValue.toString().length < 20) {
-                        let tmp = typedMinValue.toString()
-                        tmp += key.name
-                        typedMinValue = Number(tmp)
-                    }
-                } else if (key.sequence === '-') {
-                    typedMinValue = typedMinValue * -1
-                } else if (key.sequence === '+') {
-                    typedMinValue = Math.abs(typedMinValue)
-                } else {
-                    switch (key.name) {
-                        case 'backspace':
-                            if (typedMinValue.toString().length > 0) {
-                                typedMinValue = Number(typedMinValue.toString().slice(0, typedMinValue.toString().length - 1))
-                            }
-                            break
-                        case 'return':
-                            {
-                                min = typedMinValue
-                                window = "HOME"
-                            }
-                            break
-                        case 'escape':
-                            window = "HOME"
-                            break
-                        case 'q':
-                            clearInterval(valueEmitter)
-                            server.close()
-                            process.exit()
-                        default:
-                            break
-                    }
-                }
-            }
+        case 'l':
+            new InputPopup("popupTypeMin", "Type min value", min, true).show().on("confirm", (_min) => {
+                min = _min
+                GUI.warn(`NEW MIN VALUE: ${min}`)
+                drawGui()
+            })
+            break
+        case 'q':
+            closeApp()
             break
         default:
             break
     }
-    drawGui()
 })
 
 const drawGui = () => {
-    updateConsoleGauge()
-    switch (window) {
-        case "SET_SPEED":
-            addOptionPopupLayer("Set message period", periodList, selectedPeriod)
-            break
-        case "SET_MODE":
-            addOptionPopupLayer("Set simulation mode", modeList, selectedMode)
-            break
-        case "SET_MAX":
-            addInputPopupLayer("Set max value", typedMaxValue)
-            break
-        case "SET_MIN":
-            addInputPopupLayer("Set min value", typedMinValue)
-            break
-        default:
-            break
-    }
+    updateConsole()
+}
+
+const closeApp = () => {
+    console.clear()
+    clearInterval(valueEmitter)
+    server.close()
+    process.exit()
 }
 
 drawGui()
